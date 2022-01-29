e8f625b73779d01694eb220094d5bf3db961dbbe
mcarlson1021 <70486831+mcarlson1021@users.noreply.github.com>
2022-01-29 14:03:58 -0600

    Update assignment00.cc
    Added doc box

--- assignment00.cc
+++ assignment00.cc
@@ -1,1 +1,7 @@
-
+/* Mitchell Carlson
+   Z1912947
+   CSCI480-OPE1
+   
+   I certify that this is my own work and where appropriate an extenstion
+   of the starter code provided by the assignment.
+*/
